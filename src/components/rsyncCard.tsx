2b2a621142f48817b265a624f208243918000d05
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-16 12:53:55 +0000

    Moved the 'RsyncCard' component into its own file at 'components/rsyncCard'

---
+++ src/components/rsyncCard.tsx
@@ -0,0 +1,226 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  HStack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Spinner,
+  Stack,
+  StackDivider,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from '@chakra-ui/react'
+
+import { useDisclosure } from '@chakra-ui/react'
+
+import { MdDensityMedium } from 'react-icons/md'
+import { components } from 'schema/main'
+import {
+  pauseRsyncer,
+  restartRsyncer,
+  removeRsyncer,
+  finaliseRsyncer,
+  flushSkippedRsyncer,
+} from 'loaders/rsyncers'
+
+import React from 'react'
+
+type RSyncerInfo = components['schemas']['RSyncerInfo']
+
+export const RsyncCard = ({
+  rsyncer,
+  onRemove,
+  onFinalise,
+}: {
+  rsyncer: RSyncerInfo
+  onRemove: (id: number, source: string) => void
+  onFinalise: (id: number, source: string) => void
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [action, setAction] = React.useState('finalise')
+
+  const finalise = () => {
+    setAction('finalise')
+    onOpen()
+  }
+
+  const remove = () => {
+    setAction('remove')
+    onOpen()
+  }
+
+  const handleRsyncerAction = async () => {
+    if (action === 'finalise') {
+      await finaliseRsyncer(rsyncer.session_id, rsyncer.source)
+      // Run the function passed in from 'Session'
+      onFinalise(rsyncer.session_id, rsyncer.source)
+    } else if (action === 'remove') {
+      await removeRsyncer(rsyncer.session_id, rsyncer.source)
+      // Run the function passed in from 'Session'
+      onRemove(rsyncer.session_id, rsyncer.source)
+    }
+    onClose()
+  }
+
+  return (
+    <Card
+      width="100%"
+      bg={rsyncer.alive ? 'murfey.400' : '#DF928E'}
+      borderColor="murfey.300"
+    >
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            Confirm RSyncer {action}: {rsyncer.source}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Are you sure you want to continue?</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost" onClick={() => handleRsyncerAction()}>
+              Confirm
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <CardHeader>
+        <Flex>
+          {' '}
+          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+            {' '}
+            <HStack spacing="3em">
+              <Text>RSync Instance</Text>
+              {rsyncer.transferring && <Spinner color="murfey.700" />}
+              <Badge
+                colorScheme={
+                  rsyncer.tag === 'fractions'
+                    ? 'green'
+                    : rsyncer.tag === 'metadata'
+                      ? 'purple'
+                      : rsyncer.tag === 'atlas'
+                        ? 'yellow'
+                        : 'red'
+                }
+              >
+                {rsyncer.tag}
+              </Badge>
+            </HStack>
+          </Flex>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Rsync control options"
+              icon={<MdDensityMedium />}
+            />
+            <MenuList>
+              {rsyncer.alive ? (
+                <>
+                  <MenuItem
+                    onClick={() =>
+                      pauseRsyncer(rsyncer.session_id, rsyncer.source)
+                    }
+                    isDisabled={rsyncer.stopping}
+                  >
+                    Pause
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() =>
+                      flushSkippedRsyncer(rsyncer.session_id, rsyncer.source)
+                    }
+                    isDisabled={rsyncer.stopping}
+                  >
+                    Flush skipped files
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => remove()}
+                    isDisabled={rsyncer.stopping}
+                  >
+                    Stop rsync (cannot be resumed)
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      finalise()
+                    }}
+                    isDisabled={rsyncer.stopping}
+                  >
+                    Remove all source files and stop
+                  </MenuItem>
+                </>
+              ) : (
+                <>
+                  <MenuItem
+                    onClick={() =>
+                      restartRsyncer(rsyncer.session_id, rsyncer.source)
+                    }
+                  >
+                    Start
+                  </MenuItem>
+                  <MenuItem onClick={() => remove()}>Remove</MenuItem>
+                </>
+              )}
+            </MenuList>
+          </Menu>
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Source
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {rsyncer.source}
+            </Text>
+          </Box>
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Destination
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {rsyncer.destination ?? ''}
+            </Text>
+          </Box>
+          <Box>
+            <Stat>
+              <StatLabel>Transfer progress</StatLabel>
+              <StatNumber>
+                {rsyncer.num_files_transferred} transferred
+              </StatNumber>
+              <StatNumber>{rsyncer.num_files_in_queue} queued</StatNumber>
+              <StatNumber>{rsyncer.num_files_skipped} skipped</StatNumber>
+              {rsyncer.analyser_alive ? (
+                <StatNumber>
+                  {rsyncer.num_files_to_analyse} to analyse
+                </StatNumber>
+              ) : (
+                <></>
+              )}
+            </Stat>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
+  )
+}
--- src/routes/Session.tsx
+++ src/routes/Session.tsx
@@ -1,10 +1,6 @@
 import {
-  Badge,
   Box,
   Button,
-  Card,
-  CardBody,
-  CardHeader,
   FormControl,
   FormLabel,
   Flex,
@@ -13,10 +9,6 @@ import {
   HStack,
   IconButton,
   Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -26,14 +18,8 @@ import {
   ModalCloseButton,
   Select,
   Spacer,
-  Spinner,
   Stack,
-  StackDivider,
-  Stat,
-  StatLabel,
-  StatNumber,
   Switch,
-  Text,
   VStack,
   useToast,
 } from '@chakra-ui/react'
@@ -48,12 +34,7 @@ import {
   useParams,
   useNavigate,
 } from 'react-router-dom'
-import {
-  MdDensityMedium,
-  MdFileUpload,
-  MdOutlineGridOn,
-  MdPause,
-} from 'react-icons/md'
+import { MdFileUpload, MdOutlineGridOn, MdPause } from 'react-icons/md'
 import { components } from 'schema/main'
 import { getInstrumentName } from 'loaders/general'
 import { updateVisitEndTime, getSessionData } from 'loaders/sessionClients'
@@ -62,11 +43,9 @@ import { getMachineConfigData } from 'loaders/machineConfig'
 import {
   getRsyncerData,
   pauseRsyncer,
-  restartRsyncer,
   removeRsyncer,
   finaliseRsyncer,
   finaliseSession,
-  flushSkippedRsyncer,
 } from 'loaders/rsyncers'
 import { getSessionProcessingParameterData } from 'loaders/processingParameters'
 import { sessionTokenCheck, sessionHandshake } from 'loaders/jwt'
@@ -75,6 +54,7 @@ import {
   setupMultigridWatcher,
 } from 'loaders/multigridSetup'
 import { InstrumentCard } from 'components/instrumentCard'
+import { RsyncCard } from 'components/rsyncCard'
 import { UpstreamVisitCard } from 'components/upstreamVisitsCard'
 import useWebSocket from 'react-use-websocket'
 
@@ -86,193 +66,6 @@ type RSyncerInfo = components['schemas']['RSyncerInfo']
 type Session = components['schemas']['Session']
 type MachineConfig = components['schemas']['MachineConfig']
 type MultigridWatcherSpec = components['schemas']['MultigridWatcherSetup']
-
-const RsyncCard = ({
-  rsyncer,
-  onRemove,
-  onFinalise,
-}: {
-  rsyncer: RSyncerInfo
-  onRemove: (id: number, source: string) => void
-  onFinalise: (id: number, source: string) => void
-}) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [action, setAction] = React.useState('finalise')
-
-  const finalise = () => {
-    setAction('finalise')
-    onOpen()
-  }
-
-  const remove = () => {
-    setAction('remove')
-    onOpen()
-  }
-
-  const handleRsyncerAction = async () => {
-    if (action === 'finalise') {
-      await finaliseRsyncer(rsyncer.session_id, rsyncer.source)
-      // Run the function passed in from 'Session'
-      onFinalise(rsyncer.session_id, rsyncer.source)
-    } else if (action === 'remove') {
-      await removeRsyncer(rsyncer.session_id, rsyncer.source)
-      // Run the function passed in from 'Session'
-      onRemove(rsyncer.session_id, rsyncer.source)
-    }
-    onClose()
-  }
-
-  return (
-    <Card
-      width="100%"
-      bg={rsyncer.alive ? 'murfey.400' : '#DF928E'}
-      borderColor="murfey.300"
-    >
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>
-            Confirm RSyncer {action}: {rsyncer.source}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>Are you sure you want to continue?</ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="ghost" onClick={() => handleRsyncerAction()}>
-              Confirm
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-      <CardHeader>
-        <Flex>
-          {' '}
-          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
-            {' '}
-            <HStack spacing="3em">
-              <Text>RSync Instance</Text>
-              {rsyncer.transferring && <Spinner color="murfey.700" />}
-              <Badge
-                colorScheme={
-                  rsyncer.tag === 'fractions'
-                    ? 'green'
-                    : rsyncer.tag === 'metadata'
-                      ? 'purple'
-                      : rsyncer.tag === 'atlas'
-                        ? 'yellow'
-                        : 'red'
-                }
-              >
-                {rsyncer.tag}
-              </Badge>
-            </HStack>
-          </Flex>
-          <Menu>
-            <MenuButton
-              as={IconButton}
-              aria-label="Rsync control options"
-              icon={<MdDensityMedium />}
-            />
-            <MenuList>
-              {rsyncer.alive ? (
-                <>
-                  <MenuItem
-                    onClick={() =>
-                      pauseRsyncer(rsyncer.session_id, rsyncer.source)
-                    }
-                    isDisabled={rsyncer.stopping}
-                  >
-                    Pause
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() =>
-                      flushSkippedRsyncer(rsyncer.session_id, rsyncer.source)
-                    }
-                    isDisabled={rsyncer.stopping}
-                  >
-                    Flush skipped files
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => remove()}
-                    isDisabled={rsyncer.stopping}
-                  >
-                    Stop rsync (cannot be resumed)
-                  </MenuItem>
-                  <MenuItem
-                    onClick={() => {
-                      finalise()
-                    }}
-                    isDisabled={rsyncer.stopping}
-                  >
-                    Remove all source files and stop
-                  </MenuItem>
-                </>
-              ) : (
-                <>
-                  <MenuItem
-                    onClick={() =>
-                      restartRsyncer(rsyncer.session_id, rsyncer.source)
-                    }
-                  >
-                    Start
-                  </MenuItem>
-                  <MenuItem onClick={() => remove()}>Remove</MenuItem>
-                </>
-              )}
-            </MenuList>
-          </Menu>
-        </Flex>
-      </CardHeader>
-      <CardBody>
-        <Stack divider={<StackDivider />} spacing="4">
-          <Box>
-            <Heading size="xs" textTransform="uppercase">
-              Source
-            </Heading>
-            <Text pt="2" fontSize="sm">
-              {rsyncer.source}
-            </Text>
-          </Box>
-          <Box>
-            <Heading size="xs" textTransform="uppercase">
-              Destination
-            </Heading>
-            <Text pt="2" fontSize="sm">
-              {rsyncer.destination ?? ''}
-            </Text>
-          </Box>
-          <Box>
-            <Stat>
-              <StatLabel>Transfer progress</StatLabel>
-              <StatNumber>
-                {rsyncer.num_files_transferred} transferred
-              </StatNumber>
-              <StatNumber>{rsyncer.num_files_in_queue} queued</StatNumber>
-              <StatNumber>{rsyncer.num_files_skipped} skipped</StatNumber>
-              {rsyncer.analyser_alive ? (
-                <StatNumber>
-                  {rsyncer.num_files_to_analyse} to analyse
-                </StatNumber>
-              ) : (
-                <></>
-              )}
-            </Stat>
-          </Box>
-        </Stack>
-      </CardBody>
-    </Card>
-  )
-}
-
-const getUrl = (endpoint: string) => {
-  return (
-    (sessionStorage.getItem('murfeyServerURL') ??
-      process.env.REACT_APP_API_ENDPOINT) + endpoint
-  )
-}
 
 export const Session = () => {
   // ----------------------------------------------------------------------------------
