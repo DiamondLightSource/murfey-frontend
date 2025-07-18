9088cf6aa4443dd3baedfbe143571100b85c207d
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-07-18 12:43:28 +0000

    Refactored 'Home' route * Moved the SessionRow component to 'components/sessionRow' * Removed unused imports

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,194 +2,25 @@ import {
   Box,
   Button,
   Divider,
-  GridItem,
   Heading,
   HStack,
-  IconButton,
   Link,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Stack,
-  Stat,
-  StatLabel,
-  Tooltip,
   VStack,
-  useDisclosure,
 } from '@chakra-ui/react'
 import { InstrumentCard } from 'components/instrumentCard'
-import { sessionTokenCheck } from 'loaders/jwt'
-import { finaliseSession } from 'loaders/rsyncers'
-import { deleteSessionData } from 'loaders/sessionClients'
+import { SessionRow } from 'components/sessionRow'
 import React, { useEffect } from 'react'
-import { GiMagicBroom } from 'react-icons/gi'
-import { MdDelete } from 'react-icons/md'
 import {
   Link as LinkRouter,
   useLoaderData,
   useSearchParams,
   useNavigate,
 } from 'react-router-dom'
-import { PuffLoader } from 'react-spinners'
 import useWebSocket from 'react-use-websocket'
 import { components } from 'schema/main'
 import { v4 as uuid4 } from 'uuid'
 
 type Session = components['schemas']['Session']
-const SessionRow = (session: Session) => {
-  const {
-    isOpen: isOpenDelete,
-    onOpen: onOpenDelete,
-    onClose: onCloseDelete,
-  } = useDisclosure()
-  const {
-    isOpen: isOpenCleanup,
-    onOpen: onOpenCleanup,
-    onClose: onCloseCleanup,
-  } = useDisclosure()
-
-  const cleanupSession = async (sessid: number) => {
-    await finaliseSession(sessid)
-    onCloseCleanup()
-  }
-
-  const [sessionActive, setSessionActive] = React.useState(false)
-
-  useEffect(() => {
-    sessionTokenCheck(session.id).then((active) => setSessionActive(active))
-  }, [])
-
-  return (
-    <VStack w="100%" spacing={0}>
-      <Stack w="100%" spacing={5} py="0.8em">
-        {session ? (
-          <>
-            <HStack>
-              <Modal isOpen={isOpenDelete} onClose={onCloseDelete}>
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader>
-                    Confirm removing session {session.name} from list
-                  </ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody>
-                    Are you sure you want to continue? This action is not
-                    reversible
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button colorScheme="blue" mr={3} onClick={onCloseDelete}>
-                      Close
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      onClick={() => {
-                        deleteSessionData(session.id).then(() =>
-                          window.location.reload()
-                        )
-                      }}
-                    >
-                      Confirm
-                    </Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-              <Modal isOpen={isOpenCleanup} onClose={onCloseCleanup}>
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader>
-                    Confirm removing files for session {session.name}
-                  </ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody>
-                    Are you sure you want to continue? This action is not
-                    reversible
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button colorScheme="blue" mr={3} onClick={onCloseCleanup}>
-                      Close
-                    </Button>
-                    <Button
-                      variant="ghost"
-                      onClick={() => {
-                        cleanupSession(session.id)
-                      }}
-                    >
-                      Confirm
-                    </Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-              <Tooltip label={session.name}>
-                <Link
-                  key={session.id}
-                  _hover={{ textDecor: 'none' }}
-                  as={LinkRouter}
-                  display={'flex'}
-                  to={`../sessions/${session.id ?? 0}`}
-                >
-                  <Stat
-                    _hover={{
-                      borderColor: 'murfey.400',
-                    }}
-                    bg={'murfey.400'}
-                    overflow="auto"
-                    w="calc(100%)"
-                    p={2}
-                    border="1px solid grey"
-                    borderRadius={5}
-                    display={'flex'}
-                  >
-                    <HStack>
-                      <StatLabel
-                        whiteSpace="nowrap"
-                        textOverflow="ellipsis"
-                        overflow="hidden"
-                      >
-                        {session.name}: {session.id}
-                      </StatLabel>
-                      {sessionActive ? (
-                        <PuffLoader size={25} color="red" />
-                      ) : (
-                        <></>
-                      )}
-                    </HStack>
-                  </Stat>
-                </Link>
-              </Tooltip>
-              <Tooltip label="Remove from list">
-                <IconButton
-                  aria-label="Delete session"
-                  icon={<MdDelete />}
-                  onClick={onOpenDelete}
-                  isDisabled={sessionActive}
-                />
-              </Tooltip>
-              <Tooltip label="Clean up visit files">
-                <IconButton
-                  aria-label="Clean up session"
-                  icon={<GiMagicBroom />}
-                  onClick={onOpenCleanup}
-                  isDisabled={!sessionActive}
-                />
-              </Tooltip>
-            </HStack>
-          </>
-        ) : (
-          <GridItem colSpan={5}>
-            <Heading textAlign="center" py={4} variant="notFound">
-              None Found
-            </Heading>
-          </GridItem>
-        )}
-      </Stack>
-    </VStack>
-  )
-}
-
 export const Home = () => {
   // Get session data from the loader
   const sessions = useLoaderData() as {
