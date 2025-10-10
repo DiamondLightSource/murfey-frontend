cc3b89c06784d5a6b1d5ba3b4095ebc8fd54ad98
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-10-10 13:59:57 +0100

    Fixed path to the 'mime.types' file location

--- nginx/conf.d
+++ nginx/conf.d
@@ -6,7 +6,7 @@ events {
 }
 
 http {
-  include mime.types;
+  include /etc/nginx/mime.types;
   client_body_temp_path /tmp/client_temp;
   proxy_temp_path       /tmp/proxy_temp_path;
   fastcgi_temp_path     /tmp/fastcgi_temp;
@@ -25,4 +25,3 @@ http {
     }
   }
 }
-
