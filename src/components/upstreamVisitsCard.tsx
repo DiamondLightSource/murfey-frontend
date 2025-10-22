f4ff53edb8568370b36f8f8735133a79b05d40e0
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-10-22 19:01:26 +0100

    Added logic to display upstream visits according to their instrument of origin

--- src/components/upstreamVisitsCard.tsx
+++ src/components/upstreamVisitsCard.tsx
@@ -7,14 +7,60 @@ interface SessionId {
   sessid: number
 }
 
-export const UpstreamVisitCard = ({ sessid }: SessionId) => {
-  const [upstreamVisits, setUpstreamVisits] = React.useState({})
+const InstrumentUpstreamVisitsCard = ({
+  sessid,
+  instrumentName,
+  instrumentVisits,
+}: {
+  sessid: number
+  instrumentName: string
+  instrumentVisits: Record<string, string>
+}) => {
+  // Display upstream visits for a single instrument
+  // Parameters to take: instrument name and upstream visits dict
+  console.log(instrumentVisits)
+  return (
+    <Card>
+      <CardHeader>{instrumentName}</CardHeader>
+      {/* Map each visit to a button */}
+      {Object.entries(instrumentVisits).map(
+        ([visitName, visitPath]: [string, string]) => {
+          return (
+            <CardBody>
+              <Button
+                rightIcon={<MdFileDownload />}
+                onClick={() =>
+                  upstreamDataDownloadRequest(
+                    instrumentName,
+                    sessid,
+                    visitName,
+                    visitPath
+                  )
+                }
+              >
+                {visitName}
+              </Button>
+            </CardBody>
+          )
+        }
+      )}
+    </Card>
+  )
+}
 
+export const UpstreamVisitsCard = ({ sessid }: SessionId) => {
+  const [upstreamVisits, setUpstreamVisits] = React.useState<
+    Record<string, Record<string, string>>
+  >({})
+
+  // Load all visits associated with current session
   const resolveVisits = useCallback(async () => {
     const visits = await getUpstreamVisits(sessid)
+    if (!visits) return // Handle null or false-y cases
     setUpstreamVisits(visits)
     console.log(visits)
   }, [sessid])
+
   useEffect(() => {
     resolveVisits()
   }, [sessid, resolveVisits])
@@ -22,18 +68,21 @@ export const UpstreamVisitCard = ({ sessid }: SessionId) => {
   return upstreamVisits ? (
     <Card alignItems="center">
       <CardHeader>Upstream Visit Data Download</CardHeader>
-      {Object.keys(upstreamVisits).map((k) => {
-        return (
-          <CardBody>
-            <Button
-              rightIcon={<MdFileDownload />}
-              onClick={() => upstreamDataDownloadRequest(k, sessid)}
-            >
-              {k}
-            </Button>
-          </CardBody>
-        )
-      })}
+      {/* Map each instrument to its own card */}
+      {Object.entries(upstreamVisits).map(
+        ([instrumentName, instrumentVisits]: [
+          string,
+          Record<string, string>,
+        ]) => {
+          return (
+            <InstrumentUpstreamVisitsCard
+              sessid={sessid}
+              instrumentName={instrumentName}
+              instrumentVisits={instrumentVisits}
+            />
+          )
+        }
+      )}
     </Card>
   ) : (
     <></>
