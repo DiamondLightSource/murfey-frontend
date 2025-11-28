824fc20187bf23562d7fa218dd9a831f2f6d8798
Eu Pin Tien <eu-pin.tien@diamond.ac.uk>
2025-11-28 18:54:39 +0000

    Optimised Navbar component logic: * Removed unneeded pop-up menu component * Fixed Navbar height across different browser window dimensions * Reduced nesting of components

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,7 @@
 import {
   Box,
   Flex,
-  HStack,
   IconButton,
-  useDisclosure,
   Image,
   Tooltip,
   BoxProps,
@@ -12,12 +10,7 @@ import {
 import { useQueryClient } from '@tanstack/react-query'
 import { getInstrumentConnectionStatus } from 'loaders/general'
 import React from 'react'
-import {
-  MdMenu,
-  MdClose,
-  MdSignalWifi4Bar,
-  MdOutlineSignalWifiBad,
-} from 'react-icons/md'
+import { MdSignalWifi4Bar, MdOutlineSignalWifiBad } from 'react-icons/md'
 import { TbMicroscope, TbSnowflake, TbHomeCog } from 'react-icons/tb'
 import { useNavigate } from 'react-router-dom'
 
@@ -46,7 +39,6 @@ export const Navbar = ({
   const [instrumentConnectionStatus, setInsrumentConnectionStatus] =
     React.useState(false)
   const navigate = useNavigate()
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const queryClient = useQueryClient()
   const instrumentName = sessionStorage.getItem('instrumentName')
 
@@ -74,86 +66,76 @@ export const Navbar = ({
   }, [instrumentName, instrumentConnectionStatus, queryClient])
 
   return (
-    <Box position="sticky" top="0" zIndex={1} w="100%" {...props}>
+    <Box position="sticky" top="0" zIndex={1} w="100%" h={12} {...props}>
       <Flex
+        px={{
+          base: 4,
+          md: 12,
+        }}
+        py={2}
+        gap={2}
+        direction="row"
+        alignItems="center"
         bg="murfey.800"
-        px={{ base: 4, md: '2vw' }}
-        h={12}
-        alignItems={'center'}
-        justifyContent={'space-between'}
       >
-        <IconButton
-          size={'sm'}
-          icon={isOpen ? <MdClose /> : <MdMenu />}
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          bg="transparent"
-          border="none"
-          _hover={{ background: 'transparent', color: 'murfey.500' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack h="100%" spacing={4} alignItems={'center'}>
-          {logo ? (
-            <Box maxW="5rem">
-              <Image
-                alt=""
-                fit="cover"
-                paddingBottom={{ md: '6px', base: 0 }}
-                src={logo}
-              />
-            </Box>
-          ) : null}
-          <Tooltip label="Back to the Hub">
-            <IconButton
-              onClick={() => {
-                navigate(`/hub`)
-              }}
-              size={'sm'}
-              icon={
-                <>
-                  <TbHomeCog />
-                </>
-              }
-              aria-label={'Back to the Hub'}
-              _hover={{ background: 'transparent', color: 'murfey.500' }}
-            />
-          </Tooltip>
-          {/* Add the instrument name as a URL query parameter to trigger a reload */}
-          <Tooltip label="Back to the microscope">
-            <IconButton
-              onClick={() => {
-                navigate(`/home`)
-              }}
-              size={'sm'}
-              icon={
-                <>
-                  <TbSnowflake />
-                  <TbMicroscope />
-                </>
-              }
-              aria-label="Back to the microscope"
-              _hover={{ background: 'transparent', color: 'murfey.500' }}
-            />
-          </Tooltip>
-          <Tooltip
-            label={
-              instrumentConnectionStatus
-                ? 'Connected to instrument server'
-                : 'No instrument server connection'
+        {logo ? (
+          <Box maxW={24}>
+            <Image src={logo} objectFit="contain" />
+          </Box>
+        ) : null}
+        <Tooltip label="Back to the Hub">
+          <IconButton
+            onClick={() => {
+              navigate(`/hub`)
+            }}
+            size="sm"
+            icon={
+              <>
+                <TbHomeCog />
+              </>
             }
-          >
-            <span tabIndex={0}>
-              <Icon
-                as={
-                  instrumentConnectionStatus
-                    ? MdSignalWifi4Bar
-                    : MdOutlineSignalWifiBad
-                }
-                color={instrumentConnectionStatus ? 'white' : 'red'}
-              />
-            </span>
-          </Tooltip>
-        </HStack>
+            aria-label="Back to the Hub"
+            _hover={{ background: 'transparent', color: 'murfey.500' }}
+          />
+        </Tooltip>
+        {/* Add the instrument name as a URL query parameter to trigger a reload */}
+        <Tooltip label="Back to the microscope">
+          <IconButton
+            onClick={() => {
+              navigate(`/home`)
+            }}
+            size="sm"
+            icon={
+              <>
+                <TbSnowflake />
+                <TbMicroscope />
+              </>
+            }
+            aria-label="Back to the microscope"
+            _hover={{ background: 'transparent', color: 'murfey.500' }}
+          />
+        </Tooltip>
+        <Tooltip
+          label={
+            instrumentConnectionStatus
+              ? 'Connected to instrument server'
+              : 'No instrument server connection'
+          }
+        >
+          <span tabIndex={0}>
+            <Icon
+              as={
+                instrumentConnectionStatus
+                  ? MdSignalWifi4Bar
+                  : MdOutlineSignalWifiBad
+              }
+              color={instrumentConnectionStatus ? 'white' : 'red'}
+              mt={2}
+              ml={1}
+              size="sm"
+            />
+          </span>
+        </Tooltip>
       </Flex>
     </Box>
   )
