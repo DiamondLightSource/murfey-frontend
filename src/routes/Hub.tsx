c8e4f39904374ea573f9debe34f34fe82b2cef40
Tien, Eu Pin <48775880+tieneupin@users.noreply.github.com>
2026-01-08 14:46:59 +0000

    Updated Hub page rendering logic: (#65)
    * Use vertically scrolling grid layout to display supported instruments instead of a single horizontally scrolling line of Cards
    * Replaced HStack, VStack, Flex, and CardBody components with the more transferrable Box
    * Store instrument information in a React state
    * Added toggleable logic in a 'useEffect()' function to duplicate and shuffle data for rendering tests before setting the React state

--- src/routes/Hub.tsx
+++ src/routes/Hub.tsx
@@ -1,15 +1,5 @@
-import {
-  Card,
-  CardBody,
-  Flex,
-  Image,
-  Text,
-  HStack,
-  Heading,
-  VStack,
-  Box,
-} from '@chakra-ui/react'
-import { useEffect } from 'react'
+import { Card, Image, Text, Heading, Box } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { TbMicroscope, TbSnowflake } from 'react-icons/tb'
 import { useLoaderData, useNavigate } from 'react-router-dom'
 
@@ -24,8 +14,33 @@ type InstrumentInfo = {
 }
 
 export const Hub = () => {
-  const instrumentInfo = useLoaderData() as InstrumentInfo[]
   const navigate = useNavigate()
+
+  // Get data from loader and store it in a React state
+  const data = useLoaderData() as InstrumentInfo[]
+  const [instrumentInfo, setInstrumentInfo] = useState<InstrumentInfo[]>()
+  useEffect(() => {
+    let returnedData = [...data]
+
+    // Logic for duplicating + shuffling data for rendering tests
+    let duplicationCount = 0
+    let shuffle = false
+    // Duplicate the data as many times as is needed
+    if (duplicationCount > 0) {
+      for (let i = 1; i < duplicationCount + 1; i++) {
+        returnedData = returnedData.concat(data)
+      }
+    }
+    // Shuffle the data, if set
+    if (shuffle) {
+      for (let i = returnedData.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[returnedData[i], returnedData[j]] = [returnedData[j], returnedData[i]]
+      }
+    }
+
+    setInstrumentInfo(returnedData)
+  }, [data])
 
   // When first landing on this page, clear stored session info from browser
   useEffect(() => {
@@ -33,10 +48,13 @@ export const Hub = () => {
     sessionStorage.removeItem('instrumentName')
   }, [])
 
-  const navigateToInstrumentHome = (iinfo: InstrumentInfo) => {
+  const navigateToInstrumentHome = (instrumentInfo: InstrumentInfo) => {
     // Update browser session storage info
-    sessionStorage.setItem('murfeyServerURL', iinfo.instrument_url + '/')
-    sessionStorage.setItem('instrumentName', iinfo.instrument_name)
+    sessionStorage.setItem(
+      'murfeyServerURL',
+      instrumentInfo.instrument_url + '/'
+    )
+    sessionStorage.setItem('instrumentName', instrumentInfo.instrument_name)
 
     // Direct users to /login only if authenticating with 'password'
     if (process.env.REACT_APP_BACKEND_AUTH_TYPE === 'cookie') {
@@ -48,74 +66,96 @@ export const Hub = () => {
 
   return (
     // Start with a Box spanning width and height of browser window
-    <Box
-      w="100vw"
-      h="100vh"
-      overflow="hidden"
-      display="flex"
-      flexDirection="column"
-    >
+    <Box w="100vw" h="100vh" display="flex" flexDirection="column">
       {/* Title Bar */}
-      <VStack
-        alignItems="start"
-        justifyContent="center"
-        display="flex"
+      <Box
+        bg="murfey.700"
         w="100%"
-        h={24}
         px={{
           base: 8,
           md: 32,
         }}
         py={4}
-        bg="murfey.700"
+        display="flex"
+        flexDirection="column"
+        alignItems="start"
+        justifyContent="start"
+        gap={2}
       >
+        {/* Show CryoEM icons on one row */}
+        <Box
+          display="inline-flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="start"
+          fontSize="3xl"
+          color="murfey.50"
+        >
+          <TbSnowflake />
+          <TbMicroscope />
+        </Box>
         <Heading fontSize="3xl" w="100%" color="murfey.50">
-          <HStack>
-            <TbSnowflake />
-            <TbMicroscope />
-          </HStack>
           Murfey Hub
         </Heading>
-      </VStack>
+      </Box>
       {/* Main body of Hub page showing the instruments */}
-      {/* Allow horizontal overflow of instrument Cards*/}
-      <Box w="100%" flex="1" overflowX="auto" overflowY="hidden">
-        <Flex direction="row" align="stretch" h="80%" p={4} gap={4} mr={4}>
-          {instrumentInfo ? (
-            instrumentInfo.map((ini) => {
-              return (
-                <Card
-                  minW="300px"
-                  maxW="600px"
-                  h="100%"
-                  cursor="pointer"
-                  onClick={() => navigateToInstrumentHome(ini)}
+      {/* Render as a grid that overflows vertically */}
+      <Box
+        p={4}
+        w="100%"
+        flex="1"
+        display="grid"
+        gridTemplateColumns={{
+          base: 'repeat(1, minmax(200px, 600px))',
+          sm: 'repeat(2, minmax(200px, 600px))',
+          md: 'repeat(3, minmax(200px, 600px))',
+          lg: 'repeat(4, minmax(200px, 600px))',
+        }}
+        justifyContent="center"
+        overflowY="auto"
+        gap={4}
+      >
+        {instrumentInfo?.map((instrumentInfo, index) => {
+          return (
+            <Card
+              key={index}
+              w="100%"
+              minH="400px"
+              maxH="600px"
+              cursor="pointer"
+              onClick={() => navigateToInstrumentHome(instrumentInfo)}
+            >
+              <Box
+                p={4}
+                h="100%"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="space-between"
+                gap={4}
+              >
+                <Box
+                  w="100%"
+                  flex="1"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  overflow="hidden"
                 >
-                  <CardBody
-                    p={4}
-                    gap={4}
-                    display="flex"
-                    flexDirection="column"
-                    justifyContent="space-between"
-                    height="100%"
-                  >
-                    <Image
-                      src={getUrl(`instrument/${ini.instrument_name}/image`)}
-                      objectFit="contain"
-                      w="100%"
-                      h="100%"
-                    />
-                    <Text mt="auto" align="center">
-                      {ini.display_name}
-                    </Text>
-                  </CardBody>
-                </Card>
-              )
-            })
-          ) : (
-            <></>
-          )}
-        </Flex>
+                  <Image
+                    src={getUrl(
+                      `instrument/${instrumentInfo.instrument_name}/image`
+                    )}
+                    objectFit="contain"
+                    maxW="100%"
+                    maxH="100%"
+                  />
+                </Box>
+                <Text textAlign="center">{instrumentInfo.display_name}</Text>
+              </Box>
+            </Card>
+          )
+        })}
       </Box>
     </Box>
   )
